1d77ce74b1f69ad1b7c1767fe18a6b989053532c
emmybsu <74212834+emmybsu@users.noreply.github.com>
2021-04-06 11:41:20 -0400

    responsive page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
 // // The code for the chart is wrapped inside a function
 // // that automatically resizes the chart
-// function makeResponsive() {
+function makeResponsive() {
 
-//     // if the SVG area isn't empty when the browser loads, remove it
-//     // and replace it with a resized version of the chart
-//     var svgArea = d3.select("body").select("svg");
-//     if (!svgArea.empty()) {
-//       svgArea.remove();
-//     }
+    // if the SVG area isn't empty when the browser loads, remove it
+    // and replace it with a resized version of the chart
+    var svgArea = d3.select("#scatter").select("svg");
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
   
-//     // SVG wrapper dimensions are determined by the current width
-//     // and height of the browser window.
-//     var svgWidth = window.innerWidth;
-//     var svgHeight = window.innerHeight;
+    // SVG wrapper dimensions are determined by the current width
+    // and height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
 
 //comment out to address window sizing
 
     // // @TODO: YOUR CODE HERE!
-var svgWidth = 960;
-var svgHeight = 500;
+// var svgWidth = 960;
+// var svgHeight = 500;
 
 var margin = {
     top: 20,
@@ -172,4 +172,10 @@ d3.csv('/assets/data/data.csv').then((demoData) => {
 // }).catch((error) => {
 //     console.log(error);
 })
-});
+})};
+
+//call responsiveChart when page loads
+makeResponsive();
+
+//call makeResponsive when page is resized
+d3.select(window).on("resize", makeResponsive);
